08b132e66e97b5d73f96de934f6acd10665c3be1
josephSD33 <136411783+josephSD33@users.noreply.github.com>
2023-08-11 21:58:39 -0600

    avanse

--- miau.py
+++ miau.py
@@ -1,7 +1,7 @@
 import tkinter as tt
 from tkinter import messagebox
 from tkinter.ttk import *
-
+agendausuario={}
 def menuinicio():
     global ventana
     ventana = tt.Tk()
@@ -11,52 +11,63 @@ def menuinicio():
     boton2 = tt.Button(ventana, text="Ingresar a un usuario", command=accederausuario).pack()
 
     ventana.mainloop()
-    
 
 def crear():
     ventanacrear = tt.Toplevel(ventana)
-    ventanacrear.geometry("250x150")
-    global crearusuario 
+    ventanacrear.geometry("250x200")
+    global crearusuario, contrasennausuario
     crearusuario = tt.StringVar()
+    contrasennausuario = tt.StringVar()
     tt.Label(ventanacrear, text="Crear Usuario Nuevo").pack()
     tt.Label(ventanacrear, text="Nombre del usuario").pack()
     tt.Entry(ventanacrear, textvariable=crearusuario).pack()
+    tt.Label(ventanacrear, text="Contraseña").pack()
+    tt.Entry(ventanacrear, textvariable=contrasennausuario, show="*").pack()
     tt.Button(ventanacrear, text="Guardar Usuario", command=guardarusuario).pack()
-    ventana.destroy
-
-agendausuario = {}
 
 def guardarusuario():
     usuario = crearusuario.get()
-    if usuario:
+    contrasena = contrasennausuario.get()
+
+    if usuario and contrasena:
         agendausuario[usuario] = {
+            "contrasena": contrasena,
             "contactos": {},
             "tareas": [],
             "eventos": [],
         }
         messagebox.showinfo("Nuevo Usuario Guardado", f"Usuario '{usuario}' ha sido registrado exitosamente.")
-       
+    else:
+        messagebox.showerror("Error", "Por favor, complete todos los campos.")
+
+
 
 def accederausuario():
     global ventanaacceso
     ventanaacceso = tt.Toplevel(ventana)
     ventanaacceso.geometry("250x150")
-    global nombreIng 
+    global nombreIng, contrasenaIng
     nombreIng = tt.StringVar()
+    contrasenaIng = tt.StringVar()
     tt.Label(ventanaacceso, text="Acceso a Usuario").pack()
     tt.Label(ventanaacceso, text="Ingrese el Usuario").pack()
     tt.Entry(ventanaacceso, textvariable=nombreIng).pack()
+    tt.Label(ventanaacceso, text="Contraseña").pack()
+    tt.Entry(ventanaacceso, textvariable=contrasenaIng, show="*").pack()
     tt.Button(ventanaacceso, text="Ingresar", command=confirmar).pack()
 
 def confirmar():
     usuario = nombreIng.get()
-    if usuario in agendausuario:
+    contrasena = contrasenaIng.get()
+
+    if usuario in agendausuario and agendausuario[usuario]["contrasena"] == contrasena:
+        ventanaacceso.destroy() 
         ingreso()
     else:
-        messagebox.showerror("Error", f"El usuario '{usuario}' no está registrado.")
+        messagebox.showerror("Error", "Usuario o contraseña incorrectos.")
 
 def ingreso():
-    ventanamenu = tt.Toplevel(ventana)
+    ventanamenu= tt.Toplevel(ventana)
     ventanamenu.geometry("250x150")
     tt.Label(ventanamenu, text="Menu de Usuario").pack()
     tt.Button(ventanamenu, text="Ver Contactos").pack()
@@ -106,5 +117,4 @@ def agregarcontacto():
 
     else:
         messagebox.showerror("Error", f"El usuario '{usuario}' no está registrado.")
-
 menuinicio()
