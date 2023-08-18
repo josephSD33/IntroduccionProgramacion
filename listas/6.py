da7f601abd9117f262faa27c597ac84787d53058
josephSD33 <136411783+josephSD33@users.noreply.github.com>
2023-08-17 22:27:42 -0600

    k

--- listas/6.py
+++ listas/6.py
@@ -38,39 +38,4 @@ def guardar_seccion(nombre_seccion, cupos):
     else:
         messagebox.showerror("Error", "Debe ingresar un nombre de sección válido y un límite de cupos mayor a cero.")
 
-def inscribir_estudiante():
-    ventana_inscribir = tt.Toplevel(ventana)
-    ventana_inscribir.geometry("300x200")
-    nombre = tt.StringVar()
-    apellido = tt.StringVar()
-    cedula = tt.StringVar()
-    seccion_var = tt.StringVar()
-
-    tt.Label(ventana_inscribir, text="Inscribir Estudiante").pack()
-    tt.Label(ventana_inscribir, text="Nombre:").pack()
-    tt.Entry(ventana_inscribir, textvariable=nombre).pack()
-    tt.Label(ventana_inscribir, text="Apellido:").pack()
-    tt.Entry(ventana_inscribir, textvariable=apellido).pack()
-    tt.Label(ventana_inscribir, text="Cédula:").pack()
-    tt.Entry(ventana_inscribir, textvariable=cedula).pack()
-    tt.Label(ventana_inscribir, text="Sección:").pack()
-    tt.OptionMenu(ventana_inscribir, seccion_var, *secciones).pack()
-
-    tt.Button(ventana_inscribir, text="Inscribir", command=lambda: guardar_inscripcion(nombre.get(), apellido.get(), cedula.get(), seccion_var.get())).pack()
-
-def guardar_inscripcion(nombre, apellido, cedula, seccion):
-    if seccion and nombre and apellido and cedula:
-        if seccion in secciones:
-            if len(Estudiantes[seccion]) < secciones_cupos[seccion]:
-                estudiante = f"{nombre} {apellido} (Cédula: {cedula})"
-                if estudiante not in Estudiantes[seccion]:
-                    Estudiantes[seccion].append(estudiante)
-                    messagebox.showinfo("Guardado", "Estudiante inscrito exitosamente.")
-                else:
-                    messagebox.showerror("Error", "Este estudiante ya está inscrito en esta sección.")
-            else:
-                messagebox.showerror("Error", "Esta sección ha alcanzado su límite de cupos.")
-        else:
-            messagebox.showerror("Error", "Sección no encontrada.")
-    else:
-        messagebox.showerror("Error", "Todos los campos deben ser completados.")
+#lo elimine
